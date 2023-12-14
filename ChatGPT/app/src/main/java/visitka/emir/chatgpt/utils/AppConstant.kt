715ac1976f4a54d5c08936107b18b6a6219c0393
EMIR1HUB <emir-23.03@mail.ru>
2023-12-14 22:08:39 +0300

    update

--- ChatGPT/app/src/main/java/visitka/emir/chatgpt/utils/AppConstant.kt
+++ ChatGPT/app/src/main/java/visitka/emir/chatgpt/utils/AppConstant.kt
@@ -2,11 +2,6 @@ package visitka.emir.chatgpt.utils
 
 const val BASE_URL = "https://api.openai.com/v1/"
 const val CHAT_GPT_MODEL = "gpt-3.5-turbo"
-const val OPENAI_API_KEY = ""
+const val OPENAI_API_KEY = "WRITE YOUR KEY"
 
-// from Inet
-// "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"
-
-// from my
-// "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"
 
