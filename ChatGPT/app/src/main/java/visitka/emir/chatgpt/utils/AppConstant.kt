329d7982913fb8530febdd486f06ac3bd3054a74
EMIR1HUB <emir-23.03@mail.ru>
2023-12-14 22:01:15 +0300

    add apk

--- ChatGPT/app/src/main/java/visitka/emir/chatgpt/utils/AppConstant.kt
+++ ChatGPT/app/src/main/java/visitka/emir/chatgpt/utils/AppConstant.kt
@@ -4,5 +4,9 @@ const val BASE_URL = "https://api.openai.com/v1/"
 const val CHAT_GPT_MODEL = "gpt-3.5-turbo"
 const val OPENAI_API_KEY = ""
 
+// from Inet
+// "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"
 
+// from my
+// "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"
 
